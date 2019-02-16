728b95a18cbaefee73e78128f0416a33d29971ef
sanghwan <jae6120@naver.com>
2019-02-16 18:01:22 +0900

    modeling 완료

--- yangram/images/models.py
+++ yangram/images/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from yangram.users import models as user_models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # 추상클래스
+        abstract = True
+
+
+class Image(TimeStampedModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
