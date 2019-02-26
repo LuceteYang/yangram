16cdf0721e1cafa71da8c41edda1d85aad617127
sanghwan <jae6120@naver.com>
2019-02-27 05:21:53 +0900

    docker 배포 환경 설정

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://d1vljcglgvjdnq.cloudfront.net/precache-manifest.ba9a2c269d2acc9306aeb463dea5249f.js"
+  "/precache-manifest.50577134ec1b933f4a9f0513b0350211.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://d1vljcglgvjdnq.cloudfront.net/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
