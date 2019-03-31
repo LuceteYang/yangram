faf9ba09079053900c825fc5e5ef854593e69098
sanghwan <jae6120@naver.com>
2019-03-31 16:45:48 +0800

    front buildfile 배포

---
+++ frontend/build/precache-manifest.638b494ef1d613e79af369214885b326.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a363b3f57f19a1b5fcd",
+    "url": "/static/css/main.0a5d02c4.chunk.css"
+  },
+  {
+    "revision": "8a363b3f57f19a1b5fcd",
+    "url": "/static/js/main.8a363b3f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2791b050482c8b50488a",
+    "url": "/static/css/2.a166814e.chunk.css"
+  },
+  {
+    "revision": "2791b050482c8b50488a",
+    "url": "/static/js/2.2791b050.chunk.js"
+  },
+  {
+    "revision": "806d14d32a6ea4dda52960f04df5c449",
+    "url": "/static/media/logo.806d14d3.png"
+  },
+  {
+    "revision": "5d17c0469c60375a01091fcee28f3749",
+    "url": "/static/media/phone.5d17c046.png"
+  },
+  {
+    "revision": "f02d7718fb2e7a53439d4815d57b76eb",
+    "url": "/static/media/android.f02d7718.png"
+  },
+  {
+    "revision": "447dc6275013a6b1637e7b0f2fd070b9",
+    "url": "/index.html"
+  }
+];
